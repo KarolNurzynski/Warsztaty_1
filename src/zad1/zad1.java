2462fc2828b62f0149c4fa1b4f9e26094feab70d
KarolNurzynski <karolnurzynski@gmail.com>
2018-03-30 12:43:03 +0200

    First task added - zad1

---
+++ src/zad1/zad1.java
@@ -0,0 +1,40 @@
+package zad1;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class zad1 {
+	
+	public static void main(String[] args) {
+		
+		int rand = ThreadLocalRandom.current().nextInt(0,101);
+		
+		System.out.println("Zgadnij liczbe: ");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		while (true) {
+			
+			try {
+				int num = Integer.parseInt(scan.next());
+				
+				if (num>rand) {
+					System.out.println("Za duzo!");
+				} else if (num<rand) {
+					System.out.println("Za malo!");
+				} else {
+					System.out.println("Zgadles!");
+					break;
+				}
+				
+			} catch (Exception e) {
+				System.out.println("To nie jest liczba");
+			}
+		
+		}
+		
+		System.out.println("Koniec");
+
+	}
+	
+}
